d7a86cba677ed844a3999dfd20ac95cf7ebce943
MaddieAnita <maddie@mallingmay.com>
2024-06-28 17:03:58 +0100

    adds bundle identifiers

--- app.config.js
+++ app.config.js
@@ -1,8 +1,17 @@
 export default {
   android: {
     googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+    bundleIdentifier: "com.bitbybitnorthcoders.RecipeBox",
   },
   ios: {
     googleServicesFile: process.env.GOOGLE_SERVICES_PLIST,
+    bundleIdentifier: "com.bitbybitnorthcoders.RecipeBox",
+  },
+  expo: {
+    extra: {
+      eas: {
+        projectId: "84207088-82b6-41e9-b2e2-f7b4fd5baa29",
+      },
+    },
   },
 };

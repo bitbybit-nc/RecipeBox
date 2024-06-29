b4097e1691a161dcd01637594f3991acc566c71a
MaddieAnita <maddie@mallingmay.com>
2024-06-29 10:14:46 +0100

    -

--- app.config.js
+++ app.config.js
@@ -12,11 +12,6 @@ module.exports = ({ config }) => {
           process.env.GOOGLE_SERVICES_PLIST ?? "./GoogleService-Info.plist",
         bundleIdentifier: "com.bitbybitnorthcoders.RecipeBox",
       },
-      extra: {
-        eas: {
-          projectId: "84207088-82b6-41e9-b2e2-f7b4fd5baa29",
-        },
-      },
       plugins: [
         "expo-router",
         "@react-native-firebase/app",

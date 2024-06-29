efbbcfb113dbb7d0b78bdc2a4f0dce8c97e6dc64
MaddieAnita <maddie@mallingmay.com>
2024-06-29 09:01:29 +0100

    set-up configs

--- app.config.js
+++ app.config.js
@@ -1,17 +1,20 @@
-export default {
-  android: {
-    googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
-    bundleIdentifier: "com.bitbybitnorthcoders.RecipeBox",
-  },
-  ios: {
-    googleServicesFile: process.env.GOOGLE_SERVICES_PLIST,
-    bundleIdentifier: "com.bitbybitnorthcoders.RecipeBox",
-  },
-  expo: {
-    extra: {
-      eas: {
-        projectId: "84207088-82b6-41e9-b2e2-f7b4fd5baa29",
+module.exports = ({ config }) => {
+  return {
+    ...config,
+    expo: {
+      android: {
+        googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+        bundleIdentifier: "com.bitbybitnorthcoders.RecipeBox",
+      },
+      ios: {
+        googleServicesFile: process.env.GOOGLE_SERVICES_PLIST,
+        bundleIdentifier: "com.bitbybitnorthcoders.RecipeBox",
+      },
+      extra: {
+        eas: {
+          projectId: "84207088-82b6-41e9-b2e2-f7b4fd5baa29",
+        },
       },
     },
-  },
+  };
 };

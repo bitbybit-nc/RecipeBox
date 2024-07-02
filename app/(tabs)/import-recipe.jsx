5fc0c498481adecbfcb5739397a58d8fcb0cc0e6
Byteuth <francisco.shaul@gmail.com>
2024-07-02 17:12:34 +0100

    adds preview page base functionality

--- app/(tabs)/import-recipe.jsx
+++ app/(tabs)/import-recipe.jsx
@@ -4,23 +4,30 @@ import { useState } from "react";
 import { useRouter, Link } from "expo-router";
 
 export default function ImportRecipePage() {
-  const [url, setUrl] = useState("");
-  const router = useRouter()
+    const [url, setUrl] = useState("");
+    const router = useRouter();
 
-  function handleSubmit() {
-    router.push({ pathname: '../pages/recipe-preview', params: { url } })
-  }
+    function handleSubmit() {
+        router.push({ pathname: "../pages/recipe-preview", params: { url } });
+    }
 
-  return (
-    <View
-      className="flex-1 items-center justify-center bg-white"
-    >
-      <TextInput placeholder="Insert recipe URL here" value={url} onChangeText={setUrl} />
-      <Button
-        onPress= {handleSubmit}
-        title="Submit"
-        accessibilityLabel="Submit URL button"
-        />
-    </View>
-  );
+    return (
+        <View className='flex-1 items-center justify-center bg-white'>
+            <TextInput
+                    style={{
+                      backgroundColor: "#dedede",
+                      height: 30,
+                      fontSize: 13,
+                  }}
+                placeholder='Insert recipe URL here'
+                value={url}
+                onChangeText={setUrl}
+            />
+            <Button
+                onPress={handleSubmit}
+                title='Submit'
+                accessibilityLabel='Submit URL button'
+            />
+        </View>
+    );
 }

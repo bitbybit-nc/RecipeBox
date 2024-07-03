75dd12c8139994a35217874a567565cbe0d8f3b9
Byteuth <francisco.shaul@gmail.com>
2024-07-03 18:44:48 +0100

    adds recipe card base layout. revisits diateryicons view, minor layout changes

--- app/(tabs)/(importrecipe)/_layout.tsx
+++ app/(tabs)/(importrecipe)/_layout.tsx
@@ -2,7 +2,28 @@ import { Stack } from "expo-router";
 import React from "react";
 
 function Layout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="recipe-preview"
+        options={{
+          headerShown: true,
+          title: "Recipe Preview",
+        }}
+      />
+        <Stack.Screen
+          name="recipe-card"
+          options={{
+            headerShown: false,
+            title: "Recipe Card",
+          }}
+        />
+    </Stack>
+  )
 }
 
 export default Layout;
--- app/(tabs)/(importrecipe)/recipe-card.jsx
+++ app/(tabs)/(importrecipe)/recipe-card.jsx
@@ -1,38 +1,123 @@
 import { Link, useLocalSearchParams, useRoute } from "expo-router";
 import {
-  StyleSheet,
-  View,
-  Text,
-  TextInput,
-  Button,
-  FlatList,
-  Image,
-  item,
+    StyleSheet,
+    View,
+    Text,
+    TextInput,
+    Button,
+    FlatList,
+    Image,
+    item,
 } from "react-native";
 import { MultipleSelectList } from "react-native-dropdown-select-list";
 import { useState, useEffect } from "react";
 import firestore from "@react-native-firebase/firestore";
 
 export default function RecipeCard(recipe_id) {
-  const params = useLocalSearchParams();
-  const [currentRecipe, setCurrentRecipe] = useState([]);
+    const { params } = useLocalSearchParams();
+    const [currentRecipe, setCurrentRecipe] = useState([]);
+    const [dietaryImages, setDietaryImages] = useState([]);
 
+    useEffect(() => {
+        firestore()
+            .collection("Dietary-needs")
+            .get()
+            .then((dietaryCategory) => {
+                const images = {};
+                dietaryCategory.forEach((doc) => {
+                    images[doc._data["name"]] = doc._data["image-url"];
+                });
+                setDietaryImages(images);
+            })
+            .catch((err) => console.log(err));
 
-  useEffect(() => {
-    firestore()
-      .collection("Recipes")
-      .doc(`xOveArWmWclky1keMqEJ`)
-      .get()
-      .then((result) => {
-        setCurrentRecipe(result._data)
-      });
-  }, []);
+        firestore()
+            .collection("Recipes")
+            .doc(`EpnFrVebE1srTnUThVb6`)
+            .get()
+            .then((result) => {
+                setCurrentRecipe(result._data);
+            })
+            .catch((err) => console.log(err));
+    }, []);
 
-  console.log(currentRecipe, 'currentRecipe<<<<<')
+    return (
+        <View className='flex-1 items-center justify-center bg-white'>
+            <Text className='text-xl font-medium text-black'>
+                {currentRecipe.title}
+            </Text>
+            <Image
+                style={styles.midLogo}
+                source={{
+                    uri: currentRecipe["recipe-img-url"],
+                }}
+            />
+            <Text>{`URL: ${currentRecipe["source-url"]}`}</Text>
 
-  return (
-    <View>
-      <Text>Hello Recipe Card!</Text>
-    </View>
-  );
+            <View
+                style={[
+                    styles.dietaryImagesContainer,
+                    { flexDirection: "row" },
+                ]}
+            >
+                {currentRecipe["dietary-needs"] &&
+                    currentRecipe["dietary-needs"].map(
+                        (dietaryOption, index) => {
+                            const url = dietaryOption.toLowerCase();
+                            return (
+                                <View key={index}>
+                                    <Image
+                                        style={styles.tinyLogo}
+                                        source={{
+                                            uri: dietaryImages[url],
+                                        }}
+                                    />
+                                </View>
+                            );
+                        }
+                    )}
+            </View>
+
+            <View>
+                <Text className='text-xl font-medium text-black'>
+                    Cooking time:
+                </Text>
+                <Text className='text-m font-medium text-black'>
+                    {currentRecipe["cook-time"]}
+                </Text>
+            </View>
+            <View>
+                <Text className='text-xl font-medium text-black'>
+                    Ingredients:
+                </Text>
+                <Text className='text-s font-medium text-black'>
+                    {currentRecipe["ingredients"]}
+                </Text>
+            </View>
+            <View>
+                <Text className='text-xl font-medium text-black'>
+                    Cooking method:
+                </Text>
+                <Text className='text-s font-medium text-black'>
+                    {currentRecipe["cooking-method"]}
+                </Text>
+            </View>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    dietaryImagesContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "flex-start",
+    },
+    midLogo: {
+        width: 300,
+        height: 100,
+    },
+    tinyLogo: {
+        width: 50,
+        height: 50,
+    },
+});

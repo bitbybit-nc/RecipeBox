8a8a1f68f6fb1a4621518400b6dbd3a738d676a9
Darina Jurovskaja <darina@Darinas-MacBook-Pro.local>
2024-07-08 08:24:58 +0100

    Add edit profile page and update on recipe preview page for dietary options dropdown visual

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -55,7 +55,7 @@ export default function TabLayout() {
       />
 
       <Tabs.Screen
-        name="my-profile"
+        name="(profile)"
         options={{
           title: "Profile",
           tabBarIcon: ({ color, focused }) => (
